2582636487c3e9380e4110ba2fd5561f1c6a4a39
Benjamin Doherty <bendohmv@gmail.com>
2022-10-06 13:11:33 -0400

    Day1 part2

--- day1/day1.go
+++ day1/day1.go
@@ -5,9 +5,7 @@ import (
 	"fmt"
 )
 
-func Day1(input []string) bool {
-	numbers := util.ParseIntList(input)
-
+func part1(numbers []int) bool {
 	for i := 0; i < len(numbers)-1; i++ {
 		for j := i; j < len(numbers); j++ {
 			if numbers[i]+numbers[j] == 2020 {
@@ -16,6 +14,32 @@ func Day1(input []string) bool {
 			}
 		}
 	}
+	return false
+}
+func part2(numbers []int) bool {
+	for i := 0; i < len(numbers)-1; i++ {
+		for j := i; j < len(numbers); j++ {
+			remainder := 2020 - (numbers[i] + numbers[j])
+
+			if remainder > 0 && remainder < 2020 {
+				for k := j; k < len(numbers); k++ {
+					if numbers[k] == remainder {
+						fmt.Printf("2020 = %d + %d + %d, %d * %d * %d = %d",
+							numbers[i], numbers[j], numbers[k],
+							numbers[i], numbers[j], numbers[k],
+							numbers[i]*numbers[j]*numbers[k],
+						)
+						return true
+					}
+				}
+			}
+		}
+	}
 
 	return false
+}
+func Day1(input []string) bool {
+	numbers := util.ParseIntList(input)
+
+	return part1(numbers) && part2(numbers)
 }
